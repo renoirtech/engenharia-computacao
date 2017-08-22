a7920f933ce2c18b8f8f1d80298408487b504ea9
Renoir dos Reis <renofaria@gmail.com>
2017-08-22 09:44:24 -0300

    Programa 005: Progorama de resolução de equações de segundo grau.

--- n1-pgm005.cpp
+++ n1-pgm005.cpp
@@ -5,7 +5,6 @@
 * Exercicio:  Programa de equação do segundo grau
 */
 
-#include <stdlib>
 #include <iostream>
 #include <math.h>
 
@@ -17,6 +16,7 @@ int main()
 	float a, b, c, delta, xum, xdois;
 
 	// INICIALIZAÇÃO DO PROGRAMA
+	cout << "\n\n\n";
 	cout << "Renoir dos Reis \n";
 	cout << "Engenharia da Computação \n";
 	cout << "--------------------------\n";
@@ -28,24 +28,44 @@ int main()
 	cout << "Digite o valor de b: \n";
 	cin  >> b;
 	cout << "Digite o valor de c: \n";
-	cin  >> b;
+	cin  >> c;
+
+	cout << "\n\nEquação informada\n";
+	cout << a;
+	cout << "x² + ";
+	cout << b;
+	cout << "x + ";
+	cout << c;
+	cout << "\n\n";
 
 	// PROCESSAMENTO DE DADOS
-	delta = pow(b, 2) - ((4 * a) * c);
+	delta = pow(b, 2) - (4 * a * c);
+	cout << "Valor de delta\n";
+	cout << delta;
+	cout << "\n\n";
 
-	xum   = ((b * -1))/(2 * a) + (sqrt(delta)/(2 * a)) ;
-	xum   = ((b * -1))/(2 * a) - (sqrt(delta)/(2 * a)) ;
+	xum   = ((b * -1) / (2 * a)) + (sqrt(delta) / (2 * a)) ;
+	xum   = ((b * -1) / (2 * a)) - (sqrt(delta) / (2 * a)) ;
 
-	if(delta == 0)
+	if(delta > 0)
 	{
-		xum = xdois;
+		if(delta == 0)
+		{
+			xum = xdois;
+		}
+
+		// SAIDA DE INFORMAÇÕES (DADOS PROCESSADOS)
+		cout << "A Raiz x' é equivalente a: \n";
+		cout << xum;
+		cout << "\nA Raiz x\" é equivalente a: \n";
+		cout << xdois;
+	}
+	else
+	{
+		cout << "A equação não possui raizes reais\n";
 	}
 
-	// SAIDA DE INFORMAÇÕES (DADOS PROCESSADOS)
-	cout << "A Raiz x' é equivalente a: \n";
-	cout << xum;
-	cout << "\nA Raiz x\" é equivalente a: \n";
-	cout << xdois;
-	cout << "\n--------------------------\n";
-	cout << "Fim do programa \n";
+	cout << "\n\n--------------------------\n";
+	cout << "Fim do programa";
+	cout << "\n\n\n";
 }
